f3f36de7a9b474a7910e1b077bfef1a8dd3ce1c6
FZAMFIR <zamfirflorinstelian@yahoo.com>
2022-04-17 17:34:52 +0300

    tema curs 15

--- Curs15_REST_API/src/main/java/exercitii/Exercitiu1.java
+++ Curs15_REST_API/src/main/java/exercitii/Exercitiu1.java
@@ -1,19 +1,19 @@
 package exercitii;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.Curs;
+import model.Message;
 
 import java.io.File;
 import java.io.IOException;
 
+
 public class Exercitiu1 {
 
     public static void main(String[] args) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonPath = "/resoursces/example.json";
-        Curs curs =  objectMapper.readValue(new File("src/main/resources/example.json"), Curs.class);
 
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(new File("Curs15_REST_API/src/main/resources/messages.json"), Message.class);
+        System.out.println(message.toString());
 
     }
-
 }
--- Curs15_REST_API/src/main/java/model/Message.java
+++ Curs15_REST_API/src/main/java/model/Message.java
@@ -1,11 +1,27 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class Message {
     public String sender;
     public String receiver;
     public String text;
     public boolean seen;
     public Date sentDate;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "sender='" + sender + '\'' +
+                ", receiver='" + receiver + '\'' +
+                ", text='" + text + '\'' +
+                ", seen=" + seen +
+                ", sentDate=" + sentDate +
+                '}';
+    }
 }
