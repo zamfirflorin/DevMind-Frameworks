b90bf00cc0eb5875335189a4d630a5467ab096a4
WORK\florin.zamfir <florin.zamfir@bearingpoint.com>
2022-05-04 10:38:35 +0300

    added jpa repository classes and methods

--- Curs20_JpaRepository/src/main/java/application/JpaApplication.java
+++ Curs20_JpaRepository/src/main/java/application/JpaApplication.java
@@ -2,7 +2,6 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 @SpringBootApplication
 @EnableConfigurationProperties
